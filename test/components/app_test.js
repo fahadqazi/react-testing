7d6432121d3116b4861eacf7bfd094df83d35cc9
Fahad Qazi <fahadqazi@gmail.com>
2017-05-25 22:11:19 +0100

    added commentBox to app and verified with test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,12 +1,14 @@
 import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-
 describe('App', () => {
-  it('should do something', () => {
+  let component;
 
-    const component = renderComponent(App);
-    expect(component).to.contain('React simple starter')
+  beforeEach(() => {
+    component = renderComponent(App) ;
   });
 
+  it('shows a comment box', () => {
+    expect(component.find('.commentBox')).to.exist;
+  });
 });
