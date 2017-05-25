2a6821122bd9c09a2673fb98fd6d3cf95f0c62ac
Fahad Qazi <fahadqazi@gmail.com>
2017-05-25 20:43:50 +0100

    basic test

---
+++ test/components/app_test.js
@@ -0,0 +1,13 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+
+
+describe('App', () => {
+  it('should do something', () => {
+
+    const component = renderComponent(App);
+    expect(component).to.contain('React simple starter')
+  });
+
+});
