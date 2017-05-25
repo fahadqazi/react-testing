dcb93b3d8bca6840d2363f8f271099acad82c9a7
Fahad Qazi <fahadqazi@gmail.com>
2017-05-25 23:56:15 +0100

    new tests added for checking comments

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 const CommentList = (props) => {
+    const list = props.comments.map( comment => (
+        <li key={comment}>{comment}</li>
+    ))
+
     return(
        <ul className="commentList">
-
+           {list}
        </ul>
     );
 }
-export default CommentList;
+
+const mapStateToProps = (state) => {
+    return {
+        comments: state.comments
+    }
+}
+
+export default connect(mapStateToProps)(CommentList);
