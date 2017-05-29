377747bbc8451bed2fb42beeb836dff8c5d6ac78
Fahad Qazi <fahadqazi@gmail.com>
2017-05-29 23:42:14 +0100

    change tests

--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -3,9 +3,7 @@ import commentReducer from '../../src/reducers/comments';
 import { SAVE_COMMENT } from '../../src/actions/types';
 
 describe('Comments Reducer', () => {
-
     it('handles action with unknown type', () => {
-        // expect(commentReducer()).to.be.instanceof(Array);
         expect(commentReducer(undefined, {})).to.eql([]);
     });
 
@@ -13,5 +11,4 @@ describe('Comments Reducer', () => {
         const action = { type: SAVE_COMMENT, payload: 'new comment'}
         expect(commentReducer([], action)).to.eql(['new comment'])
     })
-
 });
